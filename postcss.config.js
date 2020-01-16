2bd2cd74ad41d53b1eed707332176c6726d7320f
Mark Makuno <makunomark@gmail.com>
2020-01-16 13:08:49 +0300

    Update homepage

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
-const tailwindcss = require('tailwindcss');
+const tailwindcss = require("tailwindcss");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer'),
-    ],
+  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+        serif: ["Nunito", ...defaultTheme.fontFamily.serif]
+      }
+    }
+  }
 };
